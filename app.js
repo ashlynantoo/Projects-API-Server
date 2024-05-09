6c7eeebd82bd210204daacd628eed47b2ebf4b4c
Ashlyn Antoo <ashlynantoo@gmail.com>
2024-05-09 17:16:41 -0500

    Update server for mutiple projects

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 require("express-async-errors");
 
 //express
@@ -12,18 +11,12 @@ const rateLimiter = require("express-rate-limit");
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
-const mongoSanitize = require("express-mongo-sanitize");
 
-//DB
-const connectDB = require("./db/connect");
-
-//routers
-const authRouter = require("./routes/authRoute");
-const productRouter = require("./routes/productRoute");
-const orderRouter = require("./routes/orderRoute");
+const { StatusCodes } = require("http-status-codes");
+const tours = require("./data/tours.json");
+const portfolioExperience = require("./data/portfolio-experience.json");
 
 //middleware
-const authenticateUser = require("./middleware/authentication");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
@@ -39,26 +32,26 @@ app.use(
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-app.use(mongoSanitize());
 app.use(morgan("tiny"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("<h1>E-commerce Store Server</h1>");
+  res.send("<h1>Projects API Server</h1>");
 });
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/products", productRouter);
-app.use("/api/v1/orders", authenticateUser, orderRouter);
+app.get("/tours", (req, res) => {
+  res.status(StatusCodes.OK).json(tours);
+});
+app.get("/portfolio-experience", (req, res) => {
+  res.status(StatusCodes.OK).json(portfolioExperience);
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
-    console.log("Connected to DB...");
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
